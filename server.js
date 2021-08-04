5bc99389621e82157e2aa7de38ec750be251ec51
Sebastian Kotljarevski <kvakod20@gmail.com>
2021-08-04 16:46:11 +0200

    connected to mongoDB databse, created functionality to create users, added middleware

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes/router.js";
-//import mongoose from "mongoose";
+import usersRouter from "./routes/usersRouter.js";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    }).then(() => {
+        console.log("Database connection established");
+    })
+    .catch((err) => {
+        console.log(`ERROR: ${err}`);
+    });
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +26,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
     console.log("Listening on port: " + port);
